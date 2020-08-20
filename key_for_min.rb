cbe907f6101b71967bd1c9f3ed795b19db9efd14
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-20 18:54:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.each 
 end
+
+
+#=> this method should iterate over the hash returning a key and not the value
+
+#=> the key should point to the smallest value of the set
+
+#=> if the method is called and passed an argument of an empty hash, it should return nil
